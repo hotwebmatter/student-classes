bdc3bc34ec3178ef82d33b4fdba642af529bf084
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-27 16:21:08 -0500

    Implement static method per spec
    To wit: "In the second class, create a class (static) method
    that displays name and major using the instantiated object."

--- Program.cs
+++ Program.cs
@@ -7,8 +7,16 @@ namespace student_classes
     {
         static void Main(string[] args)
         {
-            Student myStudent = new Student("Matthew", "Obert");
-            WriteLine(myStudent.GetFullName());
+            Student aStudent = new Student("Matthew", "Obert");
+            WriteLine(DisplayNameAndMajor(aStudent));
+        }
+        static string DisplayNameAndMajor(Student myStudent)
+        {
+            string result = String.Format("****************************************\n");
+            result += String.Format("* {0, 12}{1, 24} *\n", "Full Name:", myStudent.GetFullName());
+            result += String.Format("* {0, 12}{1, 24} *\n", "Major:", myStudent.Major);
+            result += String.Format("****************************************");
+            return result;
         }
     }
 }
