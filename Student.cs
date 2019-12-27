1b1c130233f6584ca561367feba05e6ae4e0de7e
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-27 15:48:25 -0500

    Implement setters and getters

--- Student.cs
+++ Student.cs
@@ -12,6 +12,61 @@ namespace student_classes
                        lastName,
                        classification,
                        major;
-
+        // accessors and mutators
+        public int StudentNumber
+        {
+            set
+            {
+                studentNumber = value;
+            }
+            get
+            {
+                return studentNumber;
+            }
+        }
+        public string FirstName
+        {
+            set
+            {
+                firstName = value;
+            }
+            get
+            {
+                return firstName;
+            }
+        }
+        public string LastName
+        {
+            set
+            {
+                lastName = value;
+            }
+            get
+            {
+                return lastName;
+            }
+        }
+        public string Classification
+        {
+            set
+            {
+                classification = value;
+            }
+            get
+            {
+                return classification;
+            }
+        }
+        public string Major
+        {
+            set
+            {
+                major = value;
+            }
+            get
+            {
+                return major;
+            }
+        }
     }
 }
