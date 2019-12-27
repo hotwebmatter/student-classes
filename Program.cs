b36a88cc991359191f7f25f619eaefcdbb61cfd9
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-27 15:04:47 -0500

    Use static System.Console

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace student_classes
 {
@@ -6,7 +7,7 @@ namespace student_classes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Hello World!");
         }
     }
 }
